6657f9e0ca0d147de9ac1fc7f9f2e09e7ad007c1
arif-hstu <arif.dvm.hstu@gmail.com>
2021-01-26 01:20:28 +0600

    simple madlibs created

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# defining the variables
+adj = input('Adjective: ')
+verb1 = input('Verb1: ')
+verb2 = input('Verb2: ')
+famous_person = input('Your famous person: ')
+
+# concatenating the strings with the variables 
+madlib = f'Computer programmig is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!'
+
+print(madlib)
